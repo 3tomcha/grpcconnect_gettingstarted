5ca02ca771f73a4ba830e8ce1c8062153bebcc3e
Tomoya Kobayashi <tomocha.marika@gmail.com>
2023-07-15 10:52:32 +0900

    connect to server

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { createPromiseClient } from "@bufbuild/connect";
 import { createConnectTransport } from "@bufbuild/connect-web";
-
+import { useState } from "react";
 import { ElizaService } from "@buf/bufbuild_eliza.bufbuild_connect-es/buf/connect/demo/eliza/v1/eliza_connect";
 
 const transport = createConnectTransport({
@@ -8,3 +8,18 @@ const transport = createConnectTransport({
 })
 
 const client = createPromiseClient(ElizaService, transport);
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  return <>
+    <form onSubmit={async (e) => {
+      e.preventDefault();
+      await client.say({ sentense: inputValue })
+    }}>
+      <input value={inputValue} onChange={e => setInputValue(e.target.value)} />
+      <button type="submit">Send</button>
+    </form>
+  </>
+}
+
+export default App;
